0454412eb927f8322f7403f10a9fdc78bbff8cbd
Purvansh Sharma <purvanshsharma6182gmail.com>
2025-03-04 10:55:46 +0530

    [Purvansh Sharma] Add. UC2 PIN Code Validation problem solved

---
+++ JS_PIN_Code_Validator_UCs/pincodevalidatoruc2.js
@@ -0,0 +1,11 @@
+function validatePinCode(pin) {
+    const regex = /^[1-9][0-9]{5}$/;
+    return regex.test(pin);
+}
+
+// Valid Test Cases
+console.log(validatePinCode("400088")); // True (Valid PIN code)
+console.log(validatePinCode("110001")); // True (Valid PIN code)
+
+// Invalid Test Cases
+console.log(validatePinCode("A400088")); // False (Starts with an alphabet)
